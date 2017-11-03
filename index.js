dbf5699a40caaea2e5a8ee91f38fa21f38d223b0
Scott Atkins <satkins16@gmail.com>
2017-11-03 12:43:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(array, name) {
-  return array.filter(function(driver) { return driver.name === name })
+  return array.filter(function(driver) { return driver === name })
 }
