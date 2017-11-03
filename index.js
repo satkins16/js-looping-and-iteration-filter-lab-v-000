7302776f9e316324a629b52b7d7deb7c3759d7eb
Scott Atkins <satkins16@gmail.com>
2017-11-03 12:45:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function findMatching(array, name) {
   return array.filter(function(driver) { return driver.toLowerCase() === name.toLowerCase() })
 }
+
+function fuzzyMatch(array, letters) {
+  
+}
